b634c48da0e77a5bf8ea7b118d9cfbf0d1aa0eaf
Lilly-25 <lilly.abraham@study.thws.de>
2024-09-02 22:03:30 +0200

    Data visualisation and faulty files removed helper scripts added

--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,10 @@ import openpyxl
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import patheffects
+from tqdm import tqdm
+import os
 
-def read_files_kpi2d(file_path, kpi2):
+def read_files_kpi2d(file_path, kpi2):##can remove this TODO less likely necessary
     ## For 2d KPI we require only 1 row of NN to plot the 2d graph
     wb = openpyxl.load_workbook(file_path)
 
@@ -77,10 +79,44 @@ def plot_kpi3d(nn, mm, eta):
 
     plt.show()
     
+def plot_testdataset_kpi2d(df_targets, df_predictions,start,end):
+    
+        nn_kpi_2d = list(range(0, 19100, 100)) # NN values alyways range from 0 to 19000 rpm
+        
+        index=df_targets.index
+
+        cols=2
+        row_height=5 
+        col_width=5
+  
+        num_plots = end - start
+
+        rows = (num_plots + cols - 1) // cols  # ceiling division to get the number of rows needed
+
+        fig, axs = plt.subplots(rows, cols, figsize=(cols * col_width, rows * row_height))  # Adjust fig size based on rows and columns
+        fig.suptitle(f'Plots of {start} to {end} Examples from the Test Dataset', fontsize=10)
+
+        for j in range(rows * cols):
+            row = j // cols
+            col = j % cols
+            current_index = (start-1) + j
+            
+            if current_index <= end and current_index < len(df_targets):
+                axs[row, col].plot(nn_kpi_2d, df_targets.loc[index[current_index]].tolist(), label='Target', color='blue')
+                axs[row, col].plot(nn_kpi_2d, df_predictions.loc[index[current_index]].tolist(), label='Predictions', color='red')
+                axs[row, col].set_xlabel('NN')
+                axs[row, col].set_ylabel('Mgrenz')
+                axs[row, col].set_title(f'Mgrenz(Torque Curve) KPI for\n{index[current_index]}', fontsize=7)
+                axs[row, col].legend()
+            else:
+                axs[row, col].axis('off')  
+        plt.tight_layout(rect=[0, 0, 1, 0.96])  
+        plt.show()
+    
 def plot_kpi2d(nn_values, mgrenz_values, predicted_mgrenz):
     plt.figure(figsize=(10, 6))
-    plt.plot(nn_values, mgrenz_values, marker='o', label='Target', color='blue')
-    plt.plot(nn_values, predicted_mgrenz, marker='o', label='Predictions', color='red')
+    plt.plot(nn_values, mgrenz_values, label='Target', color='blue')
+    plt.plot(nn_values, predicted_mgrenz, label='Predictions', color='red')
     plt.xlabel('NN Values')
     plt.ylabel('Mgrenz Values')
     plt.title(f'NN vs Mgrenz')
@@ -88,3 +124,18 @@ def plot_kpi2d(nn_values, mgrenz_values, predicted_mgrenz):
     plt.tight_layout()
     plt.legend()
     plt.show()
+    
+def remove_faulty_files(directory):
+    # Loop over all files to check if there are any faulty files and remove them
+    for filename in tqdm(os.listdir(directory)):
+        if filename.endswith(".xlsx"):
+            file_path = os.path.join(directory, filename)
+            try:
+                xls = pd.ExcelFile(file_path)
+                sheet_names = xls.sheet_names
+                if "ETA" not in sheet_names or "MM" not in sheet_names or "input_data" not in sheet_names or "NN" not in sheet_names or "Mgrenz" not in sheet_names:
+                    print(f"{filename} is missing required sheets.")
+                    os.remove(file_path)
+                    print(f"{filename} removed.")
+            except Exception as e:
+                print(f"Error reading {filename}: {e}")
