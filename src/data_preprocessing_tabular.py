3e5129d3482ffcace49d490e22c40085f3efb5ef
Lilly-25 <lilly.abraham@study.thws.de>
2024-09-18 13:32:53 +0200

    returned max_mgrenz to be used also for test dataset/new files eval

--- src/data_preprocessing_tabular.py
+++ src/data_preprocessing_tabular.py
@@ -43,57 +43,13 @@ def data_prep(test_size):
   
     return x_normalized, y1_normalized, scaler_x, scaler_y1, input_size, output_y1_size, df_x_test, df_y1_test
 
-# def dataset_creation():
-#     df_inputs=pd.read_csv('./data/TabularData2VInputs.csv')
-#     df_inputs.rename(columns={'Unnamed: 0':'filename'}, inplace=True)
-
-#     df_targets=pd.read_csv('./data/TabularData2VTargets.csv')
-#     df_targets.rename(columns={'Unnamed: 0':'filename'}, inplace=True)
-
-#     filenames = df_inputs['filename'].values##incase we need to refer the index later on
-#     x = df_inputs.drop('filename', axis=1).values
-#     y1 = df_targets.drop('filename', axis=1).values
-
-#     #TODO Use cross validation instead of train-test split
-#     x_train, x_temp, y1_train, y1_temp, filenames_train, filenames_temp = train_test_split(
-#         x, y1, filenames, test_size=0.2, random_state=42
-#     )
-#     x_val, x_test, y1_val, y1_test, filenames_val, filenames_test = train_test_split(
-#         x_temp, y1_temp, filenames_temp, test_size=0.2, random_state=42
-#     )
-
-#     # Convert test sets back to DataFrames, coz we dont want it scaled and simply want to remember what was not used for train/validation
-#     df_x_test = pd.DataFrame(x_test, columns=df_inputs.columns[1:], index=filenames_test)
-#     df_y1_test = pd.DataFrame(y1_test, columns=df_targets.columns[1:], index=filenames_test)
-    
-#     # Normalisation
-#     scaler_x = MinMaxScaler()##incase we need t do different scaling..std makes more sense for x, 
-#     scaler_y1 = MinMaxScaler()##incase we need t do different scaling..min max makes more sense for y1, 
-
-#     # Normalize the data
-#     x_train = scaler_x.fit_transform(x_train)
-#     y1_train = scaler_y1.fit_transform(y1_train)
-#     x_val = scaler_x.fit_transform(x_val)
-#     y1_val = scaler_y1.fit_transform(y1_val)
-
-    
-#     # Create PyTorch datasets
-#     train_dataset = TensorDataset(torch.FloatTensor(x_train), 
-#                                   torch.FloatTensor(y1_train))#TODO test y1 to be integer and not float..No need coz it makes it a classification problem and not a regression problem
-#     val_dataset = TensorDataset(torch.FloatTensor(x_val), 
-#                                   torch.FloatTensor(y1_val))#TODO test y1 to be integer and not float..No need coz it makes it a classification problem and not a regression problem
-    
-#     input_size = x_train.shape[1]
-#     output_y1_size = y1_train.shape[1]
-  
-#     return train_dataset, val_dataset, input_size, output_y1_size, df_x_test, df_y1_test
 
 def data_prep_eta_grid():
     
-    df_inputs=pd.read_csv('./data/CompleteTabularDataInputs.csv')
+    df_inputs=pd.read_csv('./data/TabularDataInputs.csv')
     df_inputs.rename(columns={'Unnamed: 0':'filename'}, inplace=True)
 
-    df_targets=pd.read_csv('./data/CompleteTabularDataTargets.csv')
+    df_targets=pd.read_csv('./data/TabularDataTargets.csv')
     df_targets.rename(columns={'Unnamed: 0':'filename'}, inplace=True)
 
     filenames = df_inputs['filename'].values
@@ -115,7 +71,7 @@ def data_prep_eta_grid():
     y2 = []
     # Padding
     for filename in filenames:
-        y2_file = pd.read_csv(f'./data/CompleteTabularDataETAgrid/{filename}.csv')
+        y2_file = pd.read_csv(f'./data/TabularDataETAgrid/{filename}.csv')
         # Convert DataFrame to NumPy array and replace NaNs with 1000
         #y2_values = np.nan_to_num(y2_file.values, nan=1000)
         y2_values = y2_file.values
@@ -129,14 +85,17 @@ def data_prep_eta_grid():
     
     y2 = np.array(y2)
     
-    np.save('./data/CompleteTabularDataETA.npy', y2)
+    np.save('./data/TabularDataETA.npy', y2)
 
 def data_prep_complete(test_size):
 
-    df_inputs=pd.read_csv('./data/CompleteTabularDataInputs.csv')
+    df_inputs=pd.read_csv('./data/TabularDataInputs.csv')
     df_inputs.rename(columns={'Unnamed: 0':'filename'}, inplace=True)
+    
+    # print(max(df_inputs['rad_phiv1']))
+    # print(max(df_inputs['rad_phi3b']))
 
-    df_targets=pd.read_csv('./data/CompleteTabularDataTargets.csv')
+    df_targets=pd.read_csv('./data/TabularDataTargets.csv')
     df_targets.rename(columns={'Unnamed: 0':'filename'}, inplace=True)
 
     filenames = df_inputs['filename'].values##incase we need to refer the index later on
@@ -153,6 +112,7 @@ def data_prep_complete(test_size):
     df_inputs_train_val = df_inputs[:-test_size]
     df_targets_train_val = df_targets[:-test_size]
     
+    #really no need of these
     filenames_test = filenames[-test_size:]
     filenames_train = filenames[:-test_size]
     
@@ -163,7 +123,7 @@ def data_prep_complete(test_size):
     max_mgrenz=y1.max() 
 
     
-    y2_complete = np.load('./data/CompleteTabularDataETA.npy')
+    y2_complete = np.load('./data/TabularDataETA.npy')
     y2=y2_complete[:-test_size, :, :]###only 1st dimension need to be considered..test
     ##############
     #Reshape it to 2D before scaling, then reshape back to 3D
@@ -187,7 +147,7 @@ def data_prep_complete(test_size):
 
     y2_normalized = y2_normalized.reshape(y2.shape)  # Reshape back to 3D
     
-    # Replace nans with 3
+    # Replace nans with -1..or maybe try -.1...maybe predictions might not be so skewed
     y2_normalized = np.nan_to_num(y2_normalized, nan=-1)
     
     print(np.nanmax(y2_normalized))
@@ -204,4 +164,4 @@ def data_prep_complete(test_size):
     output_y1_size = y1_normalized.shape[1]
     output_y2_size = y2_normalized.shape[1]
   
-    return x_normalized, y1_normalized, y2_normalized, scaler_x, scaler_y1, scaler_y2, input_size, output_y1_size,output_y2_size, df_x_test, df_y1_test, filenames_test, max_mgrenz
+    return x_normalized, y1_normalized, y2_normalized, scaler_x, scaler_y1, scaler_y2, input_size, output_y1_size,output_y2_size, df_x_test, df_y1_test, max_mgrenz
