d7897d9eb192e580dfc0046c80071a1d0554f9db
Lilly-25 <lilly.abraham@study.thws.de>
2024-10-02 22:36:05 +0200

    moved 2d only model training, added lr scheduler

--- src/training.py
+++ src/training.py
@@ -30,129 +30,9 @@ class LossRegularisation(nn.Module):
         return mse_loss + self.lambda_monotonic * regularized_factor
 
 
-class mlp_kpi2d_trainer:
-    def __init__(self, model, train_dataset, val_dataset, batch_size, criterion, optimizer, device=None):
-        self.model = model
-        
-        self.train_dataset = train_dataset
-        self.val_dataset = val_dataset
-        self.batch_size = batch_size
-        
-        self.criterion = criterion
-        self.optimizer = optimizer
-        self.device = device if device else torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        
-        self.model.to(self.device)
-        
-        self.train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-        self.val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-        
-        self.output_size = self.model.output_layer.out_features
-        
-        self.train_loss_history = []
-        self.val_loss_history = []
-        self.train_r2_history = []
-        self.val_r2_history = []
-
-    @staticmethod
-    def calculate_r2(outputs, labels):
-        return r2_score(labels.cpu().numpy(), outputs.cpu().numpy(), multioutput='variance_weighted')
-
-    def train(self, num_epochs):
-        for epoch in range(num_epochs):
-            train_loss, train_r2 = self._train_epoch()
-            val_loss, val_r2 = self._validate_epoch()
-            
-            self.train_loss_history.append(train_loss)
-            self.train_r2_history.append(train_r2)
-            self.val_loss_history.append(val_loss)
-            self.val_r2_history.append(val_r2)
-            
-            # print(f'Epoch {epoch+1}/{num_epochs}')
-            # print(f'Train Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}')
-            # print(f'Train R2: {train_r2:.4f}, Validation R2: {val_r2:.4f}')
-            # print('-' * 50)
-        self.training_plots()
-        return self.train_loss_history, self.train_r2_history, self.val_loss_history, self.val_r2_history
-
-    def _train_epoch(self):
-        self.model.train()
-        train_loss = 0.0
-        train_outputs = []
-        train_labels = []
-        
-        for batch_features, batch_labels in self.train_loader:
-            batch_features = batch_features.to(self.device)
-            batch_labels = batch_labels.to(self.device)
-            
-            self.optimizer.zero_grad()
-            outputs = self.model(batch_features)
-            loss = self.criterion(outputs, batch_labels)
-            loss.backward()
-            self.optimizer.step()
-            
-            train_loss += loss.item() * batch_features.size(0)
-            train_outputs.append(outputs.detach())
-            train_labels.append(batch_labels)
-        
-        train_loss /= len(self.train_dataset)
-        train_outputs = torch.cat(train_outputs)
-        train_labels = torch.cat(train_labels)
-        train_r2 = self.calculate_r2(train_outputs, train_labels)
-        
-        return train_loss, train_r2
-
-    def _validate_epoch(self):
-        self.model.eval()
-        val_loss = 0.0
-        val_outputs = []
-        val_labels = []
-        
-        with torch.no_grad():
-            for batch_features, batch_labels in self.val_loader:
-                batch_features = batch_features.to(self.device)
-                batch_labels = batch_labels.to(self.device)
-                
-                outputs = self.model(batch_features)
-                loss = self.criterion(outputs, batch_labels)
-                
-                val_loss += loss.item() * batch_features.size(0)
-                val_outputs.append(outputs)
-                val_labels.append(batch_labels)
-            
-            val_loss /= len(self.val_dataset)
-            val_outputs = torch.cat(val_outputs)
-            val_labels = torch.cat(val_labels)
-            val_r2 = self.calculate_r2(val_outputs, val_labels)
-        
-        return val_loss, val_r2
-
-    def training_plots(self):
-        """
-        Create plots for Loss and R2 score metrics.
-        """
-        metrics = [
-            ('MSE Loss', self.train_loss_history, self.val_loss_history),
-            ('R2 Score', self.train_r2_history, self.val_r2_history)
-        ]
-
-        fig, axs = plt.subplots(1, 2, figsize=(18, 6))
-        fig.suptitle('Training Metrics')
-
-        for i, (metric, train_history, val_history) in enumerate(metrics):
-            axs[i].plot(range(1, len(train_history) + 1), train_history, label='Train')
-            axs[i].plot(range(1, len(val_history) + 1), val_history, label='Validation')
-            axs[i].set_xlabel('Epoch')
-            axs[i].set_ylabel(metric)
-            axs[i].set_title(f'Train and Validation {metric}')
-            axs[i].legend()
-
-        plt.tight_layout()
-        plt.show()
-        
 class mlp_kpi3d_trainer:
     
-    def __init__(self, model, train_dataset, val_dataset, batch_size, loss, optimizer, device):
+    def __init__(self, model, train_dataset, val_dataset, batch_size, loss, optimizer, lrscheduler, device):
         
         self.model = model
         
@@ -162,6 +42,7 @@ class mlp_kpi3d_trainer:
         
         self.loss = loss
         self.optimizer = optimizer
+        self.lr_scheduler=lrscheduler
         self.device = device
         
         self.model.to(self.device)
@@ -170,11 +51,10 @@ class mlp_kpi3d_trainer:
         self.val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
         
         self.train_loss_history_y1, self.train_loss_history_y2, self.train_loss_history = [], [], []
-        self.train_score_history_y1, self.train_score_history_y2 = [],[]
+        self.train_score_history_y1, self.train_score_history_y2, self.train_score_history = [],[], []
         
-        self.val_loss_history_y1, self.val_score_history_y1  = [],[]
-        self.val_loss_history_y2, self.val_score_history_y2  = [],[]
-        self.val_loss_history = []
+        self.val_loss_history_y1, self.val_score_history_y1, self.val_loss_history = [], [],[]
+        self.val_loss_history_y2, self.val_score_history_y2, self.val_score_history = [], [],[]
 
     @staticmethod
     def y1_score(outputs, labels):
@@ -210,18 +90,19 @@ class mlp_kpi3d_trainer:
         valid_diff = np.abs(Z_diff[~np.isnan(Z_diff)])
         variance = np.mean(valid_diff ** 2)
         score = np.sqrt(variance)
-        return  score
+        return score
 
     def train(self, num_epochs):
         for _ in range(num_epochs):
-            train_loss, train_loss_y1, train_loss_y2, train_score_y1, train_score_y2 = self._train_epoch()
-            val_loss, val_loss_y1, val_loss_y2, val_score_y1, val_score_y2 = self._validate_epoch()
+            train_loss, train_loss_y1, train_loss_y2, train_score, train_score_y1, train_score_y2 = self._train_epoch()
+            val_loss, val_loss_y1, val_loss_y2, val_score, val_score_y1, val_score_y2 = self._validate_epoch()
             
             self.train_loss_history_y1.append(train_loss_y1), self.train_loss_history_y2.append(train_loss_y2)
             self.train_score_history_y1.append(train_score_y1), self.train_score_history_y2.append(train_score_y2)
             self.val_loss_history_y1.append(val_loss_y1), self.val_loss_history_y2.append(val_loss_y2)
             self.val_score_history_y1.append(val_score_y1), self.val_score_history_y2.append(val_score_y2)
             self.train_loss_history.append(train_loss), self.val_loss_history.append(val_loss)
+            self.train_score_history.append(train_score), self.val_score_history.append(val_score)
         
         return self.train_loss_history, self.val_loss_history, self.train_loss_history_y1, self.train_loss_history_y2, self.train_score_history_y1, self.train_score_history_y2, self.val_loss_history_y1, self.val_loss_history_y2, self.val_score_history_y1, self.val_score_history_y2
 
@@ -263,6 +144,8 @@ class mlp_kpi3d_trainer:
             train_outputs_y2.append(y2_pred.detach())
             train_labels_y2.append(batch_y2)
         
+        self.lr_scheduler.step()
+        
         train_loss_y1 /= len(self.train_dataset)
         train_loss_y2 /= len(self.train_dataset)
         train_loss /= len(self.train_dataset)
@@ -285,15 +168,18 @@ class mlp_kpi3d_trainer:
         
         train_score_y2 = self.y2_score(train_outputs_y2, train_labels_y2)
         
+        train_score = train_score_y1 + train_score_y2
+        
         wandb.log({
             "train_loss": train_loss,
             "train_loss_y1": train_loss_y1,
             "train_loss_y2": train_loss_y2,
             "train_score_y1": train_score_y1,
-            "train_score_y2": train_score_y2
+            "train_score_y2": train_score_y2,
+            "train_score" : train_score
         })
         
-        return train_loss, train_loss_y1, train_loss_y2, train_score_y1, train_score_y2
+        return train_loss, train_loss_y1, train_loss_y2, train_score, train_score_y1, train_score_y2
 
     def _validate_epoch(self):
         self.model.eval()
@@ -353,13 +239,16 @@ class mlp_kpi3d_trainer:
         
         val_score_y2 = self.y2_score(val_outputs_y2, val_labels_y2)
         
+        val_score = val_score_y1 + val_score_y2
+        
         wandb.log({
             "val_loss": val_loss,
             "val_loss_y1": val_loss_y1,
             "val_loss_y2": val_loss_y2,
             "val_score_y1": val_score_y1,
-            "val_score_y2": val_score_y2
+            "val_score_y2": val_score_y2, 
+            'val_score':val_score
         })
         
-        return val_loss, val_loss_y1, val_loss_y2, val_score_y1, val_score_y2
+        return val_loss, val_loss_y1, val_loss_y2,val_score, val_score_y1, val_score_y2
         
