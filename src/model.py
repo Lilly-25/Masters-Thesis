de355a48ea5b9dfcecea30d122a9a55d063b4349
Lilly-25 <lilly.abraham@study.thws.de>
2024-10-28 00:10:13 +0100

    deepened the mlp architecture

--- src/model.py
+++ src/model.py
@@ -2,7 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch_geometric.nn import HeteroConv, SAGEConv, Linear, global_mean_pool
-class mlp_kpi3d(nn.Module):
+
+class obsolete_mlp_kpi3d(nn.Module):
     def __init__(self, input_size, hidden_size, y1_shape, y2_shape, dropout_rate):
         super(mlp_kpi3d, self).__init__()
         self.shared_layers = nn.Sequential(
@@ -48,6 +49,51 @@ class mlp_kpi3d(nn.Module):
     def save(self, path):
         torch.save(self, path)
         
+class mlp_kpi3d(nn.Module):
+    def __init__(self, input_size, hidden_size, y1_shape, y2_shape, dropout_rate):
+        super(mlp_kpi3d, self).__init__()
+        self.shared_layers = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.BatchNorm1d(hidden_size),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_size, hidden_size*2),
+            nn.BatchNorm1d(hidden_size*2),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_size*2, hidden_size*4),
+            nn.BatchNorm1d(hidden_size*4),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate)
+        )
+        self.y1_layers = nn.Sequential(
+            nn.Linear(hidden_size*4, y1_shape[1]),
+            nn.ReLU()
+        )
+        self.y2_layers = nn.Sequential(
+            nn.Linear(hidden_size*4, hidden_size * 8),
+            nn.BatchNorm1d(hidden_size * 8),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_size * 8, hidden_size * 16),
+            nn.BatchNorm1d(hidden_size * 16),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_size * 16, y2_shape[1] * y2_shape[2]),
+            nn.ReLU()
+            # nn.Linear(hidden_size * 64, y2_shape[1] * y2_shape[2])
+        )
+        self.y2_shape = y2_shape
+
+    def forward(self, x):
+        shared_features = self.shared_layers(x)
+        y1_pred = self.y1_layers(shared_features)
+        y2_pred = self.y2_layers(shared_features).view(-1, *self.y2_shape[1:])
+        #TODO should i multiply the last layers with batch size
+        return y1_pred, y2_pred
+    
+    def save(self, path):
+        torch.save(self, path)
 
 class hgnn_kpi2d(torch.nn.Module):
     def __init__(self, hidden_channels, out_channels, metadata, node_dims):
